02e53818cc959d5518e4f521b63537dcc78d65d0
unknown <affulrobert1@gmail.com>
2025-10-25 00:01:25 +0000

    Calculator and styling setup

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+export default function App(){
+  return (
+    <>
+      <article>
+        <div className="screen">
+          <p className="results"> 0 </p>
+          <p className="workings"> 0 </p>
+        </div>
+        <div className="keypad">
+          <button> AC </button>
+          <button> ( </button>
+          <button> ) </button>
+          <button> &divide; </button>
+          <button> 7 </button>
+          <button> 8 </button>
+          <button> 9 </button>
+          <button> &times; </button>
+          <button> 4 </button>
+          <button> 5 </button>
+          <button> 6 </button>
+          <button> - </button>
+          <button> 1 </button>
+          <button> 2 </button>
+          <button> 3 </button>
+          <button> + </button>
+          <button> 0 </button>
+          <button> . </button>
+          <button> C </button>
+          <button> = </button>
+        </div>
+      </article>
+
+    </>
+  )
+}
